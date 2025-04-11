820f576e71b874c4b23058d12b1fc35ec8949a4c
Riddhesh <135740066+404reese@users.noreply.github.com>
2025-04-11 22:25:40 +0530

    Update Homeowners.tsx

--- src/pages/Homeowners.tsx
+++ src/pages/Homeowners.tsx
@@ -4,12 +4,14 @@ import { HomeHero } from '../components/HomeHero';
 import { ProductGrid } from '../components/ProductGrid';
 import {Sample} from '../components/Sample';
 import { Contact } from '../components/Contact';
+import { Review } from '../components/Review';
 export default function App() {
   return <main className="min-h-screen w-full">
       <Nav />
       <HomeHero />
       <ProductGrid />
       <Sample />
+      <Review />
       <Contact />
     </main>;
 }
